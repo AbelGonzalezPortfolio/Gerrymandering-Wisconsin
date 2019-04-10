01f25b920f797b8ee134c6e6cf39c605d5d91df3
grabel <30423741+grabel@users.noreply.github.com>
2019-04-09 23:11:00 -0500

    New idea for intial districts

--- Gerrymandering to 1.0/src/initial_districts.jl
+++ Gerrymandering to 1.0/src/initial_districts.jl
@@ -35,6 +35,12 @@ function add_node!(districts, dis_array, node::Int64, part_to, nodes_taken)
     push!(nodes_taken, node)
 end
 
+function add_node!(districts, dis_array, nodes::Set{Int64}, part_to, nodes_taken)
+    for i in nodes
+        add_node!(districts, dis_array, i, part_to, nodes_taken)
+    end
+end
+
 function get_democratic_share(node)
     return demographic.dem[node]/(demographic.dem[node]+demographic.rep[node])
 end
@@ -56,7 +62,7 @@ function select_node(dis_array, nodes_taken, part_to)
     if part_to == 1
         return boundary_by_dem[1]
     else
-        return boundary_by_dem[end]
+        return district_boundary
     end
 end
 
@@ -71,28 +77,51 @@ function initialize_districts(state_boundary::Array{Int64})
     districts = DistrictData(dis, dem, rep, pop)
     nodes_taken = Int64[]
 
-    # Select initial seeds
-    for i in 1:num_parts
+
+    # Select initial seed
+    # state_boundary = setdiff(state_boundary, nodes_taken)
+    # initial_seed = rand(state_boundary)
+    # add_node!(districts, dis_array, initial_seed, i, nodes_taken)
+
+    state_boundary = setdiff(state_boundary, nodes_taken)
+    initial_seed = rand(state_boundary)
+    add_node!(districts, dis_array, initial_seed, 1, nodes_taken)
+    while districts.pop[1] < parity
+        node_to_move = select_node(dis_array[1], nodes_taken, 1)
+        if node_to_move == false
+            break
+        end
+        add_node!(districts, dis_array, node_to_move, 1, nodes_taken)
+        #println(nv(graph)- length(nodes_taken))
+    end
+
+
+    for i in 2:num_parts
         state_boundary = setdiff(state_boundary, nodes_taken)
         initial_seed = rand(state_boundary)
         add_node!(districts, dis_array, initial_seed, i, nodes_taken)
     end
 
-    i = 1
-    while districts.pop[i] < (parity-1000)
-        node_to_move = select_node(dis_array[i], nodes_taken, i)
-        if node_to_move == false
-            break
+    while length(nodes_taken) != 0
+        for i in 2:num_parts
+            node_to_move = select_node(dis_array[i], nodes_taken, i)
+            if node_to_move == false
+                continue
+            end
+            add_node!(districts, dis_array, node_to_move, i, nodes_taken)
+            #println(nv(graph)- length(nodes_taken))
         end
-        add_node!(districts, dis_array, node_to_move, i, nodes_taken)
-        i = rand(collect(1:8))
-        draw_graph(graph, districts.dis, "initial_seeds")
     end
+        #draw_graph(graph, districts.dis, "$i")
+
     println("Leftover Nodes", nv(graph)-length(nodes_taken))
     return districts
 end
 
+
 state_boundary = get_state_boundary(demographic.pos)
 districts = initialize_districts(state_boundary)
-get_score(districts.dem, districts.rep)
+get_score(dis)
+
+get_lowest_district()
 draw_graph(graph, districts.dis, "initial_seeds")
